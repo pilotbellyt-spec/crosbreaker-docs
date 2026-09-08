6cd4ee45d26aafcb83189e093cc493e385d02a95
pilot bell <251052141+pilotbellyt-spec@users.noreply.github.com>
2026-09-08 00:32:01 +0000

    Restore archived colors and add navbar search

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,6 +10,12 @@ module.exports = {
   onBrokenLinks: 'throw',
   markdown: {hooks: {onBrokenMarkdownLinks: 'throw', onBrokenMarkdownImages: 'throw'}},
   plugins: [require.resolve('./plugins/doc-catalog')],
+  themes: [[require.resolve('@easyops-cn/docusaurus-search-local'), {
+    hashed: true,
+    docsRouteBasePath: '/',
+    indexBlog: false,
+    highlightSearchTermsOnTargetPage: true,
+  }]],
   presets: [
     ['classic', {
       docs: {routeBasePath: '/', sidebarPath: require.resolve('./sidebars.js')},
@@ -23,9 +29,10 @@ module.exports = {
       title: 'crosbreaker',
       items: [
         {to: '/quickstart/', label: 'Getting Started', position: 'left'},
+        {type: 'search', position: 'right'},
         {href: 'https://github.com/orgs/crosbreaker/repositories', label: 'GitHub', position: 'right'},
       ],
     },
-    colorMode: {respectPrefersColorScheme: true},
+    colorMode: {defaultMode: 'dark', respectPrefersColorScheme: false},
   },
 };
